c4558a8f6f3c0af1b2c68b843b6c7da15a7b5cd8
Charlie Szymanski <charlie.is@duke.edu>
2011-09-08 12:30:21 -0400

    Okay, so the last attempt at caching didn't work. This time I've removed the two site-wide caching middlewares and then moved to a per-view cache in the urlconf. Let's try again.

--- review_lab/apps/core/urls.py
+++ review_lab/apps/core/urls.py
@@ -1,15 +1,16 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from core import views
+from django.views.decorators.cache import cache_page
 
 urlpatterns = patterns('',
-    url(r'^review/(?P<slug>[-\w]+)/?$', views.review_view, name='review_view'),
-    url(r'^product/(?P<slug>[-\w]+)/?$', views.product_view, name='product_view'),
-    url(r'^tutorial/(?P<slug>[-\w]+)/?$', views.tutorial_view, name='tutorial_view'),
-    url(r'^challenge/(?P<slug>[-\w]+)/?$', views.challenge_view, name='challenge_view'),
-    url(r'^task/(?P<slug>[-\w]+)/$', views.task_view, name='task_view'),
-    url(r'^producttask/(?P<slug>[-\w]+)/?$', views.product_task_view, name='product_task_view'),
-    url(r'^document/(?P<slug>[-\w]+)/?$', views.document_view, name='document_view'),
+    url(r'^review/(?P<slug>[-\w]+)/?$', cache_page(60 * 15)(views.review_view), name='review_view'),
+    url(r'^product/(?P<slug>[-\w]+)/?$', cache_page(60 * 15)(views.product_view), name='product_view'),
+    url(r'^tutorial/(?P<slug>[-\w]+)/?$', cache_page(60 * 15)(views.tutorial_view), name='tutorial_view'),
+    url(r'^challenge/(?P<slug>[-\w]+)/?$', cache_page(60 * 15)(views.challenge_view), name='challenge_view'),
+    url(r'^task/(?P<slug>[-\w]+)/$', cache_page(60 * 15)(views.task_view), name='task_view'),
+    url(r'^producttask/(?P<slug>[-\w]+)/?$', cache_page(60 * 15)(views.product_task_view), name='product_task_view'),
+    url(r'^document/(?P<slug>[-\w]+)/?$', cache_page(60 * 15)(views.document_view), name='document_view'),
     url(r'^tag/(?P<slug>[-\w]+)/?$', views.tag_view, name='tag_view'),
     url(r'^more/(?P<model>\w+)/?$', views.more_view, name='more_view'),
     url(r'^search/?$', views.search_view, name='search_view'),
