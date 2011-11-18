4936bb14c0d89beb0cf39f694472d2a032f63528
Charlie Szymanski <charlie.is@duke.edu>
2011-11-17 19:53:11 -0500

    fixes to manage.py pulling _private instead of normal settings.

--- review_lab/configs/common/manage.py
+++ review_lab/configs/common/manage.py
@@ -21,11 +21,11 @@ sys.path.insert(0, python_path)
 sys.path.insert(0, apps_path)
 
 try:
-    import settings_private # Assumed to be in the same directory.
+    import settings # Assumed to be in the same directory.
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
 if __name__ == "__main__":
-    execute_manager(settings_private)
+    execute_manager(settings)
