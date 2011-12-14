435bc48bca0639f73cbfbb336f457d7d9beb7c8b
Charlie Szymanski <charlie.is@duke.edu>
2011-12-14 14:50:00 -0500

    Styling of the Review page.

---
+++ review_lab/apps/core/static/js/utils.js
@@ -0,0 +1,12 @@
+$(function(){
+    
+    /*
+    //IE Shadow Fix
+    if ($.browser.msie){
+        var $elm = $(".shadow");
+        var pos = $(elm).position();
+        $elm.after("<div class='ie-shadow'></div>");
+        $(".ie-shadow").width($(elm).width()).height($(elm).height()).css("left", (pos.left + 5) + "px").css("top",(pos.top + 5) + "px");
+    }
+    */
+});
