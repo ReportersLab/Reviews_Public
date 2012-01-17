284a3818b294119a502a4a35a94a91e034cba84b
Charlie Szymanski <charlie.is@duke.edu>
2012-01-17 12:43:21 -0500

    reduced size of textbox on homepage for IE 8. added script for html5 'placeholder' support in IE

--- review_lab/apps/core/static/js/utils.js
+++ review_lab/apps/core/static/js/utils.js
@@ -1,12 +1,34 @@
 $(function(){
     
-    /*
+    
     //IE Shadow Fix
     if ($.browser.msie){
-        var $elm = $(".shadow");
+        var $elm = $(".box_shadow");
         var pos = $(elm).position();
         $elm.after("<div class='ie-shadow'></div>");
         $(".ie-shadow").width($(elm).width()).height($(elm).height()).css("left", (pos.left + 5) + "px").css("top",(pos.top + 5) + "px");
     }
-    */
+    
+});
+
+
+
+ $(function() {
+	if(!$.support.placeholder) { 
+		var active = document.activeElement;
+		$(':text').focus(function () {
+			if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+				$(this).val('').removeClass('hasPlaceholder');
+			}
+		}).blur(function () {
+			if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+				$(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+			}
+		});
+		$(':text').blur();
+		$(active).focus();
+		$('form').submit(function () {
+			$(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
+		});
+	}
 });
