16024cec14df5923cc7ef831709f2e84b9aab2e8
Charlie Szymanski <charlie.is@duke.edu>
2012-01-17 12:45:11 -0500

    removed box-shadow fix attempt. Caused JS bugs in IE

--- review_lab/apps/core/static/js/utils.js
+++ review_lab/apps/core/static/js/utils.js
@@ -1,15 +1,3 @@
-$(function(){
-    
-    
-    //IE Shadow Fix
-    if ($.browser.msie){
-        var $elm = $(".box_shadow");
-        var pos = $(elm).position();
-        $elm.after("<div class='ie-shadow'></div>");
-        $(".ie-shadow").width($(elm).width()).height($(elm).height()).css("left", (pos.left + 5) + "px").css("top",(pos.top + 5) + "px");
-    }
-    
-});
 
 
 
