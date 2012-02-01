d037760d87622151a7115f7cd07e7dfc0e9f14ea
Charlie Szymanski <charlie.is@duke.edu>
2012-02-01 10:47:35 -0500

    made backup script export a different file every day of the week.

--- data/psql/backup_db.sh
+++ data/psql/backup_db.sh
@@ -1,2 +1,2 @@
-pg_dump review_lab > review_lab.dump.out
-s3cmd put review_lab.dump.out s3://media.reporterslab.org/beta/review_lab/backup/
+pg_dump review_lab > "review_lab.dump.out.$(date +%w)"
+s3cmd put "review_lab.dump.out.$(date +%w)" s3://media.reporterslab.org/beta/review_lab/backup/
